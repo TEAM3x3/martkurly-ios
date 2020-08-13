71fe9f424166522a3bc2754dd3dd30924e645088
1000JI <chjh1992@gmail.com>
2020-08-13 21:52:36 +0900

    feat: Xcode Init MartCurly

---
+++ martkurly/martkurly/Supporting/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  martkurly
+//
+//  Created by 천지운 on 2020/08/13.
+//  Copyright © 2020 Team3x3. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = ViewController()
+        window?.backgroundColor = .systemBackground
+        window?.makeKeyAndVisible()
+        
+        return true
+    }
+}
+
---
+++ martkurly/martkurly/Supporting/SceneDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  SceneDelegate.swift
+//  martkurly
+//
+//  Created by 천지운 on 2020/08/13.
+//  Copyright © 2020 Team3x3. All rights reserved.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+}
+
