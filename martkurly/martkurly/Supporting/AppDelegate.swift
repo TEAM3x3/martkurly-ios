b7368b049b138539d98b029ff59bf5f9af4c8de6
1000JI <chjh1992@gmail.com>
2020-08-19 14:33:04 +0900

    feat: 메인 탭바 및 홈뷰 컨트롤러 추가

--- martkurly/martkurly/Supporting/AppDelegate.swift
+++ martkurly/martkurly/Supporting/AppDelegate.swift
@@ -15,8 +15,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
-        window?.rootViewController = ViewController()
-        window?.backgroundColor = .systemBackground
+
+        let controller = MainTBC()
+        window?.rootViewController = controller
+
         window?.makeKeyAndVisible()
 
         return true
---
+++ martkurly/martkurly/Utils/Extensions.swift
@@ -0,0 +1,33 @@
+//
+//  Extensions.swift
+//  martkurly
+//
+//  Created by 천지운 on 2020/08/19.
+//  Copyright © 2020 Team3x3. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    convenience init(red: Int, green: Int, blue: Int) {
+        assert(red >= 0 && red <= 255, "Invalid red component")
+        assert(green >= 0 && green <= 255, "Invalid green component")
+        assert(blue >= 0 && blue <= 255, "Invalid blue component")
+
+        self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1.0)
+    }
+
+    convenience init(rgb: Int) {
+        self.init(
+            red: (rgb >> 16) & 0xFF,
+            green: (rgb >> 8) & 0xFF,
+            blue: rgb & 0xFF
+        )
+    }
+
+    static let martkurlyMainPurpleColor = UIColor(red: 85, green: 0, blue: 114)
+}
+
+extension UIImage {
+    static let martcurlyMainTitleWhiteImage = UIImage(named: "Martcurly_MainTitle_White")?.withRenderingMode(.alwaysOriginal)
+}
