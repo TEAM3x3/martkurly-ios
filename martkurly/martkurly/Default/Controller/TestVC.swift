2d18961e3e9a9a9f8ad480632ef88b7ab0e2c11a
KasRoid <KasRoid@gmail.com>
2020-08-20 02:01:55 +0900

    feat: KurlyButton 구현 완료

--- martkurly/martkurly/Default/Controller/TestVC.swift
+++ martkurly/martkurly/Default/Controller/TestVC.swift
@@ -11,7 +11,7 @@ import UIKit
 class TestVC: UIViewController {
 
     // MARK: - Properties
-    let sampleView = WhyKurlyView()
+    let sampleView = KurlyButton(title: "구매하기", style: .white)
 
     // MARK: - Lifecycle
     override func viewDidLoad() {
@@ -26,7 +26,7 @@ class TestVC: UIViewController {
             $0.top.equalTo(view.safeAreaLayoutGuide).offset(50)
             $0.centerX.equalToSuperview()
             $0.leading.trailing.equalToSuperview().inset(20)
-//            $0.height.equalTo(300)
+            $0.height.equalTo(55)
         }
     }
 }
