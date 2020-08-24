4eb3befcb1d5ea16aaa88536c246d167c7c01045
1000JI <chjh1992@gmail.com>
2020-08-24 19:15:58 +0900

    feat: 이벤트 페이지 일부 구현

--- martkurly/martkurly/Home/View/EventListCell.swift
+++ martkurly/martkurly/Home/View/EventListCell.swift
@@ -14,6 +14,17 @@ class EventListCell: UICollectionViewCell {
 
     static let identifier = "EventListCell"
 
+    private lazy var eventTableView = UITableView().then {
+        $0.rowHeight = 160
+        $0.backgroundColor = .white
+
+        $0.dataSource = self
+        $0.delegate = self
+
+        $0.register(EventCell.self,
+                    forCellReuseIdentifier: EventCell.identifier)
+    }
+
     // MARK: - LifeCycle
 
     override init(frame: CGRect) {
@@ -28,6 +39,75 @@ class EventListCell: UICollectionViewCell {
     // MARK: - Helpers
 
     func configureUI() {
-        self.backgroundColor = .systemPink
+        self.backgroundColor = .white
+
+        self.addSubview(eventTableView)
+        eventTableView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+}
+
+// MARK: - UITableViewDataSource
+
+extension EventListCell: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 5
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: EventCell.identifier, for: indexPath) as! EventCell
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return 28
+    }
+}
+
+extension EventListCell: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        return UIView()
+    }
+}
+
+// MARK: - EventCell
+
+class EventCell: UITableViewCell {
+
+    // MARK: - Properties
+
+    static let identifier = "EventCell"
+
+    private let eventImageView = UIImageView().then {
+        $0.backgroundColor = .white
+        $0.contentMode = .scaleAspectFill
+        $0.clipsToBounds = true
+
+        $0.image = UIImage(named: "TestImage")
+    }
+
+    // MARK: - LifeCycle
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        configureUI()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - Helpers
+
+    func configureUI() {
+        self.addSubview(eventImageView)
+        eventImageView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
     }
 }
