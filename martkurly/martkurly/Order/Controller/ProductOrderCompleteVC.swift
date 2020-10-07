75e4286bdf8d6cbef07cbd5bacb379550c1054c9
shooooting <shooootingsta@gmail.com>
2020-10-08 07:44:21 +0900

    ordercompleteVC complete

--- martkurly/martkurly/Order/Controller/ProductOrderCompleteVC.swift
+++ martkurly/martkurly/Order/Controller/ProductOrderCompleteVC.swift
@@ -11,11 +11,16 @@ import UIKit
 class ProductOrderCompleteVC: UIViewController {
 
     // MARK: - Properties
+    let checkView = ProductOrderCheckView()
+    let line = UIView()
+
+    private let goHomeButton = KurlyButton(title: "홈으로 이동", style: .purple)
 
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         setConfigure()
+        setUI()
     }
 
     override func viewWillAppear(_ animated: Bool) {
@@ -27,8 +32,50 @@ class ProductOrderCompleteVC: UIViewController {
     }
 
     // MARK: - UI
-
     private func setConfigure() {
+        [checkView, line, goHomeButton].forEach {
+            view.addSubview($0)
+        }
 
+        view.backgroundColor = ColorManager.General.backGray.rawValue
+        line.backgroundColor = ColorManager.General.mainGray.rawValue
+
+        checkView.name = "" // 유저 이름
+        checkView.payment = 10000 // 결제 금액
+
+        checkView.orderListButton.addTarget(self, action: #selector(orderListButtonTap(_:)), for: .touchUpInside)
+        goHomeButton.addTarget(self, action: #selector(goHomeButtonTap(_:)), for: .touchUpInside)
+    }
+
+    @objc
+    func orderListButtonTap(_ sender: UIButton) {
+        print("주문내역 상세보기")
+    }
+
+    @objc
+    func goHomeButtonTap(_ sender: UIButton) {
+        print("홈으로 가기")
+    }
+
+    private func setUI() {
+        checkView.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide)
+            $0.leading.equalToSuperview().offset(8)
+            $0.trailing.equalToSuperview().inset(8)
+            $0.bottom.equalTo(goHomeButton.snp.top).offset(8)
+            $0.centerX.equalToSuperview()
+        }
+
+        line.snp.makeConstraints {
+            $0.top.leading.trailing.equalTo(view.safeAreaLayoutGuide)
+            $0.height.equalTo(1)
+        }
+
+        goHomeButton.snp.makeConstraints {
+            $0.bottom.equalToSuperview().inset(40)
+            $0.leading.equalToSuperview().offset(8)
+            $0.width.equalTo(checkView)
+            $0.height.equalTo(55)
+        }
     }
 }
